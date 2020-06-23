b25b4f8225fb308fde87c067210e8190ede4c068
Felix Stein <mail@flxn.de>
2020-06-23 14:51:42 +0200

    fixes

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import os
 import json
 from sseclient import SSEClient as EventSource
 from dotenv import load_dotenv
+from pprint import pprint
 load_dotenv()
 
 # init tweepy
@@ -20,8 +21,13 @@ for event in EventSource(url):
             pass
         else:
             if change['user'].startswith("130.83."):
-                diff_link = "{}/w/index.php?diff={}&oldid={}".format(change['server_url'], change['revision']['new'], change['revision']['old'])
-                tweet_message = '"{}" wurde anonym aus dem Netz der TU Darmstadt bearbeitet:\n{}'.format(change[title], diff_link)
-                print(tweet_message)
-                print()
-                api.update_status(tweet_message)
+                if 'revision' in change:
+                    if 'old' in change['revision']:
+                        diff_link = "{}/w/index.php?diff={}&oldid={}".format(change['server_url'], change['revision']['new'], change['revision']['old'])
+                        tweet_message = '"{}" wurde anonym aus dem Netz der TU Darmstadt bearbeitet:\n{}'.format(change['title'], diff_link)
+                    else:
+                        article_link = change['meta']['uri']
+                        tweet_message = '"{}" wurde anonym aus dem Netz der TU Darmstadt angelegt:\n{}'.format(change['title'], article_link)
+                    print(tweet_message)
+                    print()               
+                    twitter.update_status(tweet_message)
